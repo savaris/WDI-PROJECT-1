ac7e184da1559286e4c50407bc6f5930f170f588
savaris <hudhayfajamalkhan@gmail.com>
2016-12-13 18:26:15 +0000

    Added Main Them With Updates

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 var Game = Game || {};
 var firstChoice;
 var secondChoice;
+var score = 0;
 
 Game.pickBoxes = function() {
   $(this).toggleClass('card');
   $(this).html('<img class="cardImage" src="images/image' + $(this).attr('id') + '.png"/>');
-  $(this).css('backgroundColor', 'white');
+  $(this).css('backgroundColor', '');
   if (!firstChoice){
     firstChoice = $(this).attr('id');
   }  else {
@@ -15,10 +16,9 @@ Game.pickBoxes = function() {
 };
 
 Game.match = function(){
-  console.log('running');
   if (firstChoice === secondChoice){
-    // alert('Match');
     $('#display').html('Match');
+    $('#score').text(++score);
   } else {
     var $lis = $('li');
     for (var i = 0; i < $lis.length; i++) {
@@ -30,7 +30,6 @@ Game.match = function(){
         }, 1000);
       }
     }
-    // alert('Try Again');
     $('#display').html('Try Again');
   }
   firstChoice = '';
@@ -51,6 +50,7 @@ Game.reset = function(){
 };
 
 Game.createBoard = function(){
+  score = 0;
   for (var i = 0; i < this.boardBase * this.boardBase; i++) {
     $('ul').append('<li class="card" id='+ this.items[i] +'></li>');
   }
@@ -71,9 +71,9 @@ Game.shuffleArray = function(a) {
 Game.start = function(){
   this.boardBase = 6;
   this.items     = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15,16,16,17,17,18,18];
-
   this.shuffleArray(this.items);
   this.addListeners();
 };
+
 
 $(Game.start.bind(Game));
